c15b5151641e514b0b68b549a2401a2131ff4cad
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-09-19 16:09:51 -0300

    Exercicio de numero par ou impar

---
+++ Curso_Java/NumeroPar.java
@@ -0,0 +1,11 @@
+public class NumeroPar{
+	public static void main(String[] args) {
+        int num = 1;
+        
+        if (num % 2 == 0) {
+        System.out.println("O numero é par!");
+        } else {
+        System.out.println("O numero é impar!");
+        }
+	}
+}
