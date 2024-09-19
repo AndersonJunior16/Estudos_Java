b24e05f985bf574c4799102062612c33c0f634b0
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-09-19 15:31:19 -0300

    Exercicio calculo de triangulo

---
+++ Curso_Java/CalculadoraAreaRetangulo.java
@@ -0,0 +1,9 @@
+public class CalculadoraAreaRetangulo{
+	public static void main(String[] args) {
+        double altura = 5.0d;
+        double largura = 10d;
+        var area = altura * largura;
+        
+        System.out.println("Area do retangulo: " + area);
+	}
+}
