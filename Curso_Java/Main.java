8dda0820f1126c44e10e6cf1d6fd48e9cb8722f5
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-09-27 16:11:20 -0300

    Exercicio de métodos e modificador .main

---
+++ Curso_Java/Main.java
@@ -0,0 +1,9 @@
+import exercicio.dados.*;
+
+public class Main {
+  public static void main(String args[]) {
+      Produto prod = new Produto("Camiseta", 89.90, 20);
+      System.out.println(prod.printinfo());
+
+  }
+}
